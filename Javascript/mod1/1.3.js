58f9ee2238a12a367831855c34963eeb428249a1
Niconiso <nico.jokinen96@gmail.com>
2024-11-12 14:30:15 +0200

    lorem ipsum

--- Javascript/mod1/1.3.js
+++ Javascript/mod1/1.3.js
@@ -1,9 +1,9 @@
 function laskuri() {
-  const num1 = (prompt("Anna ensimmäinen numero:"));
-  const num2 = (prompt("Anna toinen numero:"));
-  const num3 = (prompt("Anna kolmas numero:"));
-  const sum = num1 + num2 + num3;
-  const product = num1 * num2 * num3;
+  const num1 = (parseFloat(prompt("Anna ensimmäinen numero:")));
+  const num2 = (parseFloat(prompt("Anna toinen numero:")));
+  const num3 = (parseFloat(prompt("Anna kolmas numero:")));
+  const sum = (num1) + (num2) + (num3);
+  const product = (num1) * (num2) * (num3);
   const average = (num1 + num2 + num3) / 3;
   console.log("Sum:", sum);
   console.log("Product:", product);
