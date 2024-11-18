1bd828b3ae19cde60f63f319ffc49bbb0e7d7236
Niconiso <nico.jokinen96@gmail.com>
2024-11-18 10:00:33 +0200

    lorem ipsum

--- Javascript/mod2/2.3.js
+++ Javascript/mod2/2.3.js
@@ -1,6 +1,6 @@
 let dogs = [];
 for (let i = 0; i < 6; i++) {
-   dogs.push(prompt("Anna koiran nimi " + (i + 1) + ":"));
+   dogs.push(prompt("Anna koiran nimi :" + (i + 1) ));
 }
 dogs.sort().reverse();
 console.log("Koirat käänteisaakkosjärkässä:");
--- Javascript/mod2/2.6.js
+++ Javascript/mod2/2.6.js
@@ -1,16 +1,7 @@
-function rollDice() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
 let rolls = [];
-let result;
-do {
-  result = rollDice();
-  rolls.push(result);
-} while (result !== 6);
-
-console.log('<ul>');
-for (let roll of rolls) {
-  console.log(`<li>${roll}</li>`);
+while (true) {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  rolls.push(roll);
+  if (roll === 6) break;
 }
-console.log('</ul>');
+rolls.forEach(roll => console.log(`<li>${roll}</li>`));
